63cba8d7ba55bdeff90deed3f9571ea27f0b10c9
Erik Lundmark <mail@elundmark.se>
2014-05-31 15:34:49 +0200

    Re-added legacy files

---
+++ tz-aio.meta.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name          Torrentz All-in-One
+// @description   Does everything you wish Torrentz.eu could do!
+// @version       2.5.9
+// @date          2014-05-31
+// @author        elundmark
+// @contact       mail@elundmark.se
+// @license       CC0 1.0 Universal; http://creativecommons.org/publicdomain/zero/1.0/
+// @namespace     http://elundmark.se/code/tz-aio/
+// @homepage      https://github.com/elundmark/tz-aio-userscript
+// @updateURL     http://elundmark.se/_files/js/tz-aio/tz-aio.meta.js
+// @downloadURL   http://elundmark.se/_files/js/tz-aio/tz-aio.user.js
+// @supportURL    https://github.com/elundmark/tz-aio-userscript/issues
+// @require       https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @require       https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js
+// @require       https://cdn.jsdelivr.net/jquery.spectrum/1.3.3/spectrum.js
+// @resource css1 https://cdn.jsdelivr.net/jquery.spectrum/1.3.3/spectrum.css
+// @resource css2 http://elundmark.se/_files/js/tz-aio/tz-aio-style-2.css?v=2-5-9-0
+// @include       /^https?://(?:www\.)?(?:tz\.ai|torrents\.de|torrentz-proxy\.com|(?:torrentz\.(?:eu|ph|li|com|me|in|hk|ch)))/.*/
+// @exclude       /^https?://[^/]+/feed(?:_[a-zA-Z]+)?\?.*/
+// @exclude       /^https?://[^/]+/announcelist_.*/
+// @exclude       /^https?://[^/]+/report_.*/
+// @exclude       /^https?://[^/]+/comment_.*/
+// @grant         GM_info
+// @grant         GM_addStyle
+// @grant         GM_log
+// @grant         GM_getResourceText
+// @grant         GM_setValue
+// @grant         GM_getValue
+// @grant         GM_deleteValue
+// @grant         GM_openInTab
+// @grant         GM_getMetadata
+// @grant         GM_setClipboard
+// ==/UserScript==
